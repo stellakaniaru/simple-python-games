b629bb1e22952626a4bd94062eca7ed4f671fd73
Stella Kaniaru <stellakaniaru16@gmail.com>
2017-04-10 18:09:49 +0300

    Applying python 3 convections

--- guessing_game.py
+++ guessing_game.py
@@ -4,34 +4,34 @@ import random
 
 guessesTaken = 0
 
-print "Hello! What is your name?"
+print ("Hello! What is your name?")
 myName = input()
 
 number = random.randint(1,30)
-print "We'll %s, I am thinking of a number between 1 and 30" %myName
+print( "We'll %s, I am thinking of a number between 1 and 30" %myName)
 
 while guessesTaken < 10:
-	print "Take a guess."
+	print ("Take a guess.")
 	guess = input()
 	guess = int(guess)
 
 	guessesTaken += 1
 
 	if guess < number:
-		print "Your guess is too low!"
+		print ("Your guess is too low!")
 
 	if guess > number:
-		print "Your guess is too high!"
+		print( "Your guess is too high!")
 
 	if guess == number:
 		break
 
 if guess == number:
 	guessesTaken = str(guessesTaken)
-	print "Good job %s! You guessed my number in %s guesses!" %(myName, guessesTaken)
+	print( "Good job %s! You guessed my number in %s guesses!" %(myName, guessesTaken))
 
 if guess != number:
 	number = str(number)
-	print "Nope. The number I was thinking of was %s" %number
+	print ("Nope. The number I was thinking of was %s" %number)
 
 

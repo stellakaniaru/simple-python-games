b9a4c1c8b0ce898a2d089957d54fbcee8952f4e0
Stella Kaniaru <stellakaniaru16@gmail.com>
2017-04-10 18:12:36 +0300

    A program telling jokes

---
+++ jokes.py
@@ -0,0 +1,22 @@
+'''Telling jokes.'''
+
+#the input function expects a string.Can be anything. Makes the program interactive.
+print("What do you get when you cross a snowman with a vampire?")
+input()
+print("Frostbite!")
+
+#escape character for an apostrophe
+print("What do dentists call an atronaut\'s cavity?")
+input()
+print("A black hole!")
+
+print("Knock knock.")
+input()
+print("Who's there?")
+input()
+print("Interrupting cow.")
+input()
+
+#the comma tells python that the next print command should be executed on the current line, not a new line.
+print("Interrupting cow  wh",)
+print("-MOO!")
